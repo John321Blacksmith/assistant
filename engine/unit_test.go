d30ac0496a57093171ba8da154516c0431915d12
Ivan Kuzmichev <ivan.kuzmichev@red-soft.ru>
2026-06-27 15:55:40 +0300

    feat: add template for unit tests

---
+++ engine/unit_test.go
@@ -0,0 +1,15 @@
+package engine
+
+import (
+	"testing"
+)
+
+func TestDatasetRefinement(t *testing.T) {}
+
+func TestUniqueElementsIntersection(t *testing.T) {}
+
+func TestFreqDictGreatestContext(t *testing.T) {}
+
+func TestKnownDataStructWork(t *testing.T) {}
+
+func TestUnknownDataStructWork(t *testing.T) {}
