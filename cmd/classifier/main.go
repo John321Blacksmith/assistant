4525cd536db0c7f3330d9f405dec5a8a6d46f1fb
John321Blacksmith <kuzmichevi321@gmail.com>
2026-07-29 22:20:42 +0300

    feat: implemnted grpc server

--- cmd/classifier/main.go
+++ cmd/classifier/main.go
@@ -6,28 +6,50 @@ package main
 
 import (
 	"classifier/engine"
+	pb "classifier/proto"
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
-func main() {
-	// rawText := `Yields on German benchmark 10-year bonds rose seven basis points to 3.07%, their highest in nearly a month and climbing for an eighth straight day in the longest streak since January. French debt suffered even more, also weighed down by political uncertainty following Marine Le Pen’s decision to run in next year’s presidential election. US 10-year yields were two basis points higher at 4.57%.
-	// 			tire, фитн, gym, muscle, water, форма, спорт, вода, мышmuscle, график.run, exercis, schedule, упражн, питат, nutri, здоров, sport, shape, health, rest
-	// 			In the final, the seeded under the ninth number of Noskova outplayed her compatriot, the tenth racket of the tournament Karolina Mukhova, with a score of 6:2, 5:7, 6:3. The meeting lasted 2 hours 28 minutes. In the second set, Mukhova played five matches. `
+type ClassifierServer struct {
+	classifier *engine.Classifier
+	pb.UnimplementedClassifierServer
+}
 
-	rawText2 := `
-	Министерство иностранных дел России уведомило недружественные страны Запада о неприемлемости размещения войск "коалиции желающих" на территории Украины. Если эти страны отправят солдат на украинские территории - ВС РФ будут рассматривать их как законные цели, препятствующие достижению задач спецоперации.
-	`
+func (s *ClassifierServer) GetMainContext(ctx context.Context, in *pb.BotRequest) (*pb.Response, error) {
+	slog.Info("classifier ->", "GetMainContext", "PROCESSING DATA")
+	fmt.Println(s.classifier.GetMainContext())
+	sentences := s.classifier.ProcessInput(in.Input)
+	s.classifier.RecognizeSentences(sentences)
+	mainContext := s.classifier.GetMainContext()
+	return &pb.Response{Output: fmt.Sprintf("You are talking about %s", mainContext)}, nil
+}
+
+func main() {
 	dataManager := engine.NewDataManager("./categories.json")
 	dataset, err := dataManager.LoadDataset()
 	if err != nil {
-		log.Fatalf("Could not load dataset: %s", err)
+		log.Fatalf("Couldn't load the dataset: %s", err)
 	}
 	classifier := engine.NewClassifier(dataset)
-	sentences := classifier.ProcessInput(rawText2)
-	fmt.Println(sentences)
-	classifier.RecognizeSentences(sentences)
-	fmt.Println("recognized:  ", engine.GetKnownData(classifier))
-	fmt.Println("not recognized:  ", engine.GetUnknownData(classifier)[0].GetData())
-
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Error setting up listener: %s", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterClassifierServer(
+		s,
+		&ClassifierServer{
+			classifier: classifier,
+		},
+	)
+	slog.Info("Classifier Server is running on port", "port", listener.Addr().String())
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Error setting up the Classifier Server: %s", err)
+	}
 }
