bc04c337a88c16dd45f4bd877da87ce4e75511d9
John321Blacksmith <kuzmichevi321@yandex.ru>
2026-09-12 00:07:42 +0300

    feat: add

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,35 @@
+import os
 import grpc
+from dotenv import load_dotenv
 from proto import classifier_pb2, classifier_pb2_grpc
+from vkbottle.bot import Bot, Message
 
 
-def main():
-    with grpc.insecure_channel("localhost:50051") as ch:
+load_dotenv()
+
+
+token = os.getenv("BOT_TOKEN")
+classifierIP = os.getenv("GRPC_IP")
+classifierPort = os.getenv("GRPC_PORT")
+
+if not token:
+    raise RuntimeError("Bot token wasn't provided")
+
+
+bot = Bot(token=token)
+
+
+@bot.on.message()
+async def handle_message(message: Message):
+    response: str
+
+    with grpc.insecure_channel(f"{classifierIP}:{classifierPort}") as ch:
         clientStub = classifier_pb2_grpc.ClassifierStub(ch)
-        while True:
-            inp = input(">>> ")
-            request = classifier_pb2.BotRequest(input=inp)
-            response = clientStub.GetMainContext(request)
-            print("response from the target localhost:50051: ", response)
+        request = classifier_pb2.BotRequest(input=message.text)
+        response = clientStub.GetMainContext(request)
+
+    return await message.answer(message=f'you are talking about {response}')
 
 
 if __name__ == '__main__':
-    main()
+    bot.run()
